d59219a7d2bc28c97a71e8a2071f2285e1b82b7e
tutich <pkoinange02@gmail.com>
2024-02-16 23:58:34 +0300

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Index';
+
 
 function App() {
   return (
     <div className="App">
-      <p>hello world</p>
+
     </div>
   );
 }
--- frontend/src/pages/Home/Index.jsx
+++ frontend/src/pages/Home/Index.jsx
@@ -1,9 +1,20 @@
-import About from './Pages/Home/About';
+import About from './Pages/About';
+import HomePage from './Pages/HomePage';
+import Skills from './Pages/Skills';
+import Contact from './Pages/Conntact';
+import Resume from './Pages/Resume';
+import Portfolio from './Pages/Portfolio';
 
 
 function Home() {
     return (
         <>
+        <HomePage />
+        <About />
+        <Skills />
+        <Portfolio />
+        <Resume />
+        <Contact />
         </>
     )
 }
